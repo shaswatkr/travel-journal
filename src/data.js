3561b71e0edde5fd258ea7419c9f33347787a51b
Shaswat <shaswatkr96@gmail.com>
2022-07-01 23:02:34 +0530

    Creating the mock Data file

---
+++ src/data.js
@@ -0,0 +1,11 @@
+import japanImg from "./assets/japan.jfif";
+import australiaImg from "./assets/australia.jfif";
+import norwayImg from "./assets/norway.jfif";
+
+const data = [
+    { id: 1, img: japanImg, country: "Japan", locationName: "Mount Fuji", date: "12 Jan,2021 - 24 Jan, 2021", description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists." },
+    { id: 2, img: australiaImg, country: "Australia", locationName: "Sydney Opera House", date: "27 May, 2021 - 8 Jun, 2021", description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings." },
+    { id: 3, img: norwayImg, country: "Norway", locationName: "Geirangerfjord", date: "01 Oct, 2021 - 18 Nov, 2021", description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality." }
+];
+
+export default data;
