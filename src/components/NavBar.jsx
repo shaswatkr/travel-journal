4d741168ed93a607a6ab25a26ed15f3d854a37a4
Shaswat <shaswatkr96@gmail.com>
2022-07-01 23:03:11 +0530

    Created the NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>
+                <i className="bi bi-globe"></i>
+                {" "}my travel journal
+            </h1>
+        </nav>
+    );
+};
+
+export default NavBar;
